243dcb04d1dfee59d466875a4a011f88c18b1161
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-26 23:51:08 +0300

    Commit 1. test

--- mane.py
+++ mane.py
@@ -5,3 +5,6 @@ def mane_function(x):
     # Comment for Commit 2. master
 	dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
     list_of_smth = [dictionary, 1234, None, f'String with {x}']
+
+def test_function(y):
+    t = int(input('Number:'))
