c007ddb36e0e8d985dab983dcfd8eefc7fcfdd50
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-26 20:56:37 +0300

    Commit from PyCharm

--- mane.py
+++ mane.py
@@ -3,7 +3,14 @@ print("Hello")
 print("World!")
 print("This is 4 commit")
 print("This is 5 commit")
+print("New line")
 
 # second
 print("Commit second 1")
 print("Commit second 2")
+
+# New line
+print("New line")
+print("New line")
+print("New line")
+print("New line")
