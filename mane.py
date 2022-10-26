0d6888e70d047a4e35fcb80f6c45b7d5d16fc9a3
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-26 23:44:54 +0300

    Commit 3. master

--- mane.py
+++ mane.py
@@ -3,3 +3,5 @@ def mane_function(x):
     for i in range(10):
         print(i**2)
     # Comment for Commit 2. master
+	dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
+    list_of_smth = [dictionary, 1234, None, f'String with {x}']
