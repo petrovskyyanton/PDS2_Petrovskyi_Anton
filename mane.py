a44aee1f3c26b9375be1a737bb13eeed1d9f22b3
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-10-27 00:02:19 +0300

    Commit 6. add file py_charm.py

--- mane.py
+++ mane.py
@@ -3,16 +3,17 @@ def mane_function(x):
     for i in range(10):
         print(i**2)
     # Comment for Commit 2. master
-	dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
+    dictionary = {'key_1': 1, 'key_2': 2, 'key_3': 3}
     list_of_smth = [dictionary, 1234, None, f'String with {x}']
-	# Comment for Commit 4. master
+    # Comment for Commit 4. master
     set_of_smth = (1, 3, 24, f'{x}')
     return list_of_smth, set_of_smth
 
 
 def test_function(y):
     t = int(input('Number:'))
-	a = t//10
+    a = t//10
     print(f'String with {y} and {a}')
-	
+
+
 print(test_function(1), mane_function(1))
